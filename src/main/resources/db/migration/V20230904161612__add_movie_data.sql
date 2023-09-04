4ec9fc29d60db4226f892afc5b751494c6bd2bf0
Gerard Kamran Kalambacal <kamrangerardkalambacal@gmail.com>
2023-09-04 21:43:09 +0800

    refactor: updated columns for add_movie_data

--- src/main/resources/db/migration/V20230904161612__add_movie_data.sql
+++ src/main/resources/db/migration/V20230904161612__add_movie_data.sql
@@ -1,11 +1,11 @@
-INSERT INTO movies (movie_title) VALUES
-    ('The Shawshank Redemption'),
-    ('The Godfather'),
-    ('The Dark Knight'),
-    ('Pulp Fiction'),
-    ('Schindler''s List'),
-    ('Forrest Gump'),
-    ('Fight Club'),
-    ('The Matrix'),
-    ('Goodfellas'),
-    ('The Silence of the Lambs');
+INSERT INTO movies (movie_title, is_available) VALUES
+    ('The Shawshank Redemption',true),
+    ('The Godfather',true),
+    ('The Dark Knight',true),
+    ('Pulp Fiction',true),
+    ('Schindler''s List',true),
+    ('Forrest Gump',true),
+    ('Fight Club',true),
+    ('The Matrix',true),
+    ('Goodfellas',false),
+    ('The Silence of the Lambs',true);
